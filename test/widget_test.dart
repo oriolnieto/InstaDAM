2aba8a371a7dadb77ecc072c60067706efa2aa43
Oriol Nieto <oriolnieto2006@gmail.com>
2026-01-08 15:41:07 +0100

    Commit Inicial
    He creat tots el arxius necesseraris (de moment)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:instadam/main.dart';
+import 'package:instadam/feed.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
